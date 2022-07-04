3d349103fe9da30e1524dfed230b4b6bc42c15f1
Asim <assemayman136@gmail.com>
2022-07-04 17:01:02 +0200

    frist task last edition

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        sayHi(getName());
+
+    }
+    static String getName(){
         System.out.println("Enter your name");
         Scanner scanner =new Scanner(System.in);
-        String name= scanner.nextLine();
+        return scanner.nextLine();
+    }
+    static void sayHi(String name){
         System.out.printf("Hi %s",name);
-
     }
 }
